f0490ce5c91afa9828ff16339c90948ae233ee3c
Tasnimul Hasan <121094458+TasnimulHasan0@users.noreply.github.com>
2025-05-12 00:32:28 +0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      // This is needed for Same's preview environment
+      clientPort: 443,
+      protocol: 'wss'
+    }
+  }
+});
